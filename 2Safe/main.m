67dc8a9ac90a906c3544e186aece1bfa99cf6793
Kakysha <ezhikvdele@gmail.com>
2012-11-29 15:11:51 +0400

    Initial Commit

---
+++ 2Safe/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  2Safe
+//
+//  Created by Drunk on 29.11.12.
+//  Copyright (c) 2012 zaopark. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
