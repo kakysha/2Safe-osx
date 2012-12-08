246c96d5f8675e0b139b37349da7a26951e385b3
Danila Yakovlev <dang1214@gmail.com>
2012-12-09 01:45:40 +0400

    FileHandler class created.

---
+++ 2Safe/FileHandler.m
@@ -0,0 +1,13 @@
+//
+//  FileHandler.m
+//  2Safe
+//
+//  Created by Dan on 12/9/12.
+//  Copyright (c) 2012 zaopark. All rights reserved.
+//
+
+#import "FileHandler.h"
+
+@implementation FileHandler
+
+@end
