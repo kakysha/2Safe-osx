aa4f6646535e3e876451274231980e94c2501a8a
Danila Yakovlev <dang1214@gmail.com>
2013-01-08 23:32:24 +0400

    Two methods added: - Get MD5 hash of a file - Get unix time of a file

--- 2Safe/FileHandler.m
+++ 2Safe/FileHandler.m
@@ -9,6 +9,10 @@
 #import "FileHandler.h"
 #import "FileUploader.h"
 
+#include <CoreFoundation/CoreFoundation.h>
+#include <CommonCrypto/CommonDigest.h>
+#define FileHashDefaultChunkSizeForReadingData 4096
+
 static void callback(ConstFSEventStreamRef streamRef,void *clientCallBackInfo,size_t numEvents,
                      void *eventPaths,const FSEventStreamEventFlags eventFlags[],
                      const FSEventStreamEventId eventIds[])
@@ -67,8 +71,93 @@ static void callback(ConstFSEventStreamRef streamRef,void *clientCallBackInfo,si
     FSEventStreamStart(_stream);
 }
 
-//-(void)setDelegate:(NSObject<FolderEventDelegate> *)del{
-//    delegate = del;
-//}
+NSString* getFileMD5hash(NSString* filePath) {
+    
+    // Declare needed variables
+    CFStringRef result = NULL;
+    NSString *ns_result = NULL;
+    CFReadStreamRef readStream = NULL;
+    
+    // Get the file URL
+    CFURLRef fileURL =
+    CFURLCreateWithFileSystemPath(kCFAllocatorDefault,
+                                  (CFStringRef)filePath,
+                                  kCFURLPOSIXPathStyle,
+                                  (Boolean)false);
+    if (!fileURL) return NULL;
+    
+    // Create and open the read stream
+    readStream = CFReadStreamCreateWithFile(kCFAllocatorDefault,
+                                            (CFURLRef)fileURL);
+    if (!readStream) {
+        CFRelease(fileURL);
+        return NULL;
+    }
+    bool didSucceed = (bool)CFReadStreamOpen(readStream);
+    if (!didSucceed) {
+        CFReadStreamClose(readStream);
+        CFRelease(readStream);
+        CFRelease(fileURL);
+        return NULL;
+    }
+    
+    // Initialize the hash object
+    CC_MD5_CTX hashObject;
+    CC_MD5_Init(&hashObject);
+    
+    // Feed the data to the hash object
+    bool hasMoreData = true;
+    while (hasMoreData) {
+        uint8_t buffer[FileHashDefaultChunkSizeForReadingData];
+        CFIndex readBytesCount = CFReadStreamRead(readStream,
+                                                  (UInt8 *)buffer,
+                                                  (CFIndex)sizeof(buffer));
+        if (readBytesCount == -1) break;
+        if (readBytesCount == 0) {
+            hasMoreData = false;
+            continue;
+        }
+        CC_MD5_Update(&hashObject,
+                      (const void *)buffer,
+                      (CC_LONG)readBytesCount);
+    }
+    
+    // Check if the read operation succeeded
+    didSucceed = !hasMoreData;
+    
+    // Compute the hash digest
+    unsigned char digest[CC_MD5_DIGEST_LENGTH];
+    CC_MD5_Final(digest, &hashObject);
+    
+    // Abort if the read operation failed
+    if (!didSucceed) {
+        CFReadStreamClose(readStream);
+        CFRelease(readStream);
+        CFRelease(fileURL);
+        return NULL;
+    }
+    
+    // Compute the string result
+    char hash[2 * sizeof(digest) + 1];
+    for (size_t i = 0; i < sizeof(digest); ++i) {
+        snprintf(hash + (2 * i), 3, "%02x", (int)(digest[i]));
+    }
+    result = CFStringCreateWithCString(kCFAllocatorDefault,
+                                       (const char *)hash,
+                                       kCFStringEncodingUTF8);
+    
+    CFReadStreamClose(readStream);
+    CFRelease(readStream);
+    CFRelease(fileURL);
+    return ns_result = (__bridge NSString*) result;
+}
+
+- (NSString*) getModificationData: (NSString*) path {
+    NSDictionary* fileAttribs = [[NSFileManager defaultManager] attributesOfItemAtPath:path error:nil];
+    NSDate *date = [fileAttribs fileModificationDate];
+    NSTimeInterval ti = [date timeIntervalSince1970];
+    NSString *result = [NSString stringWithFormat: @"%f", ti];
+    return result;
+}
 
 @end
