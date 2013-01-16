9ce52e92c45a771f7a13cb1cadc76a0ba7c4e193
Danila Yakovlev <dang1214@gmail.com>
2013-01-17 00:39:25 +0400

    Making getClientQueues method.

--- 2Safe/Synchronization.m
+++ 2Safe/Synchronization.m
@@ -9,29 +9,46 @@
 #import "Synchronization.h"
 #import "FileHandler.h"
 #import "NSMutableArray+Stack.h"
+#import "Database.h"
+#import "FSElement.h"
 
 @implementation Synchronization{
     NSMutableArray *_stack;
+    NSMutableArray *clientInsertionsQueue;
+    NSMutableArray *clientDeletionsQueue;
 }
 
 -(NSMutableDictionary*) getClientQueue:(NSString*) folder {
-    NSMutableDictionary *clientQueue = [NSMutableDictionary dictionaryWithCapacity:20];
+    clientInsertionsQueue = [NSMutableArray arrayWithCapacity:20];
+    clientDeletionsQueue = [NSMutableArray arrayWithCapacity:20];
+    Database *db = [Database databaseForAccount:@"kakysha"];
     _stack = [NSMutableArray arrayWithCapacity:100];
-    [_stack addObject:folder];
+    FSElement *firstElem = [[FSElement alloc] initWithPath:folder];
+    firstElem.id = @"1";
+    [_stack addObject:firstElem];
     while([_stack count] != 0){
+        FSElement *stackElem = [_stack pop];
+        NSArray* bdfiles = [db childElementsOfId:[stackElem id]];
         NSFileManager *fm = [NSFileManager defaultManager];
         NSError *err;
-        NSArray* files = [fm contentsOfDirectoryAtPath:[_stack pop] error:&err];
+        NSArray* files = [fm contentsOfDirectoryAtPath:stackElem.filePath error:&err];
+        
         
         for(NSString *file in files) {
-            NSString *path = [[_stack pop] stringByAppendingPathComponent:file];
+            NSString *path = [stackElem.filePath stringByAppendingPathComponent:file];
             BOOL isDir = NO;
             [fm fileExistsAtPath:path isDirectory:(&isDir)];
+            FSElement *elementToAdd = [[FSElement alloc] initWithPath:path];
             if(isDir){
-                [_stack push:path];
+                [_stack push:elementToAdd];
             }
-            //!!!!
-        }
+            NSUInteger foundIndex = [bdfiles indexOfObjectPassingTest:^(id obj, NSUInteger idx, BOOL *stop){if ([obj name] == elementToAdd.name){return YES;}else{return NO;}}];
+            if (foundIndex == NSNotFound) {
+                [clientInsertionsQueue addObject:elementToAdd];
+            }
+            else {
+                if (elementToAdd.mdate == bdfiles[foundIndex])
+            }
 
     }
     
